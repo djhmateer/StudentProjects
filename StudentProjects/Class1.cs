0232898887f0f2752b112cdebb220d6b171fe168
Dave Mateer home <davemateer@gmail.com>
2014-09-29 11:38:14 +0100

    Making the StudentProjects solver with iterative version of recursion

---
+++ StudentProjects/Class1.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using System;
+using System.Diagnostics;
+
+namespace StudentProjects {
+    public class NestedLoopsIterativeStudent {
+        int numberOfNestedLoops;
+        int numberOfIterations;
+        int[] sequenceOfValues;
+        int lastPositionInArray;
+
+        [Test]
+        public void DoSomething() {
+            numberOfNestedLoops = 3;
+            numberOfIterations = 3;
+            lastPositionInArray = numberOfNestedLoops - 1;
+            sequenceOfValues = new int[numberOfNestedLoops];
+            Solve();
+        }
+
+        public void Solve() {
+            var projects = new[] { 1, 2, 3 };
+            var listOfStudents = new List<Student>();
+            var student0 = new Student { ProjectChoiceA = 1, ProjectChoiceB = 2, ProjectChoiceC = 3, ProjectWinner = 0 };
+            var student1 = new Student { ProjectChoiceA = 2, ProjectChoiceB = 3, ProjectChoiceC = 1, ProjectWinner = 0 };
+            var student2 = new Student { ProjectChoiceA = 3, ProjectChoiceB = 1, ProjectChoiceC = 2, ProjectWinner = 0 };
+            listOfStudents.Add(student0);
+            listOfStudents.Add(student1);
+            listOfStudents.Add(student2);
+
+            InitLoops();
+            int lowestScore = 99999;
+            while (true) {
+                PrintLoops();
+
+                // get score for this permutation eg 1 2 3
+                // pick a project for each student
+                var projectForStudent0 = sequenceOfValues[0];
+                var projectForStudent1 = sequenceOfValues[1];
+                var projectForStudent2 = sequenceOfValues[2];
+
+                if (projectForStudent0 != projectForStudent1 &&
+                    projectForStudent1 != projectForStudent2 &&
+                    projectForStudent2 != projectForStudent0)
+                {
+                    // get score for this permutation
+                    int score = 0;
+                    if (projectForStudent0 == listOfStudents[0].ProjectChoiceA) score += 1;
+                    if (projectForStudent0 == listOfStudents[0].ProjectChoiceB) score += 2;
+                    if (projectForStudent0 == listOfStudents[0].ProjectChoiceC) score += 3;
+
+                    if (projectForStudent1 == listOfStudents[1].ProjectChoiceA) score += 1;
+                    if (projectForStudent1 == listOfStudents[1].ProjectChoiceB) score += 2;
+                    if (projectForStudent1 == listOfStudents[1].ProjectChoiceC) score += 3;
+
+                    if (projectForStudent2 == listOfStudents[2].ProjectChoiceA) score += 1;
+                    if (projectForStudent2 == listOfStudents[2].ProjectChoiceB) score += 2;
+                    if (projectForStudent2 == listOfStudents[2].ProjectChoiceC) score += 3;
+
+                    // is this the lowest score, remember which projects
+                    if (score <= lowestScore && score >= 3) {
+                        lowestScore = score;
+                        listOfStudents[0].ProjectWinner = projectForStudent0;
+                        listOfStudents[1].ProjectWinner = projectForStudent1;
+                        listOfStudents[2].ProjectWinner = projectForStudent2;
+                    }
+                }
+
+               
+                int currentPositionInSOV = lastPositionInArray;
+                sequenceOfValues[currentPositionInSOV] += 1;
+
+                while (sequenceOfValues[currentPositionInSOV] > numberOfIterations) {
+                    sequenceOfValues[currentPositionInSOV] = 1;
+                    currentPositionInSOV--;
+
+                    if (currentPositionInSOV < 0) return; // break out of while
+                    sequenceOfValues[currentPositionInSOV] += 1;
+                }
+            }
+        }
+
+        public void PrintLoops() {
+            var message = "";
+            for (int i = 0; i < numberOfNestedLoops; i++) {
+                message += String.Format("{0} ", sequenceOfValues[i]);
+            }
+            Debug.WriteLine(message);
+        }
+
+        public void InitLoops() {
+            for (int i = 0; i < numberOfNestedLoops; i++) {
+                sequenceOfValues[i] = 1;
+            }
+        }
+    }
+}
